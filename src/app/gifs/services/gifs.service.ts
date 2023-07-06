6ede69a939d0ecf41d0d767a74acacb46fa040b0
YohaEsperanza <yoernest.96@gmail.com>
2023-07-06 16:52:59 -0600

    mostrando los gif en la aplicaion

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
+  public gifsList:Gif[] = [];
+
   private _tagsHistory: string[] = []
   private apiKey:string = 'ZmIlPJrXehwrv6yFKNDp1fWT5r70ibu8';
+  private serviceUrl:string = 'http://api.giphy.com/v1/gifs';
   constructor(private http:HttpClient) { }
 
   get tagsHistory():string[]{
@@ -28,7 +32,17 @@ export class GifsService {
 
     this.organizeHistory(tag);
 
-    this.http.get('http://api.giphy.com/v1/gifs/trending?api_key=ZmIlPJrXehwrv6yFKNDp1fWT5r70ibu8&q=valorant&limit=10');
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', 10)
+    .set('q', tag)
+
+
+    this.http.get<SearchResponse>(`${ this.serviceUrl }/search`, {params})
+    .subscribe( (resp) => {
+      this.gifsList  = resp.data;
+      console.log({gifs:this.gifsList});
+    });
     // const resp = await fetch('http://api.giphy.com/v1/gifs/trending?api_key=ZmIlPJrXehwrv6yFKNDp1fWT5r70ibu8&q=valorant&limit=10');
     // const data = await resp.json();
     // console.log(data);
